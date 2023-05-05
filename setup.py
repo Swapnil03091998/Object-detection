a474a63da728c19835957ce27456120a4d11d435
Swapnil03091998 <102348803+Swapnil03091998@users.noreply.github.com>
2023-05-05 18:25:04 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from cx_Freeze import setup, Executable
+
+setup(name="Object Detection Simple Software",
+      version="0.1",
+      description="This software detects objects in real time",
+      executables=[Executable("main.py")])/Users/ankit/PycharmProjects/pythonProject/source code/setup.py
